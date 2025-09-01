8320bbf20178bdbbd19336b12913ef88f5701d97
HollowDobt <2901448780@qq.com>
2025-09-01 11:35:28 +0800

    Rewrite algorithm max seq [9]

--- linked-list/llist.cpp
+++ linked-list/llist.cpp
@@ -1,10 +1,9 @@
-#include <stdexcept>
 #include <cstddef>
+#include <stdexcept>
 
 template <typename T>
 class LinkedList {
    private:
-
     struct Node {
        private:
         T _value;
@@ -32,7 +31,8 @@ class LinkedList {
 
        public:
         // 构造函数
-        Iterator(Node* ptr, Node* head, Node* foot) : _ptr(ptr), _head(head), _foot(foot) {}
+        Iterator(Node* ptr, Node* head, Node* foot)
+            : _ptr(ptr), _head(head), _foot(foot) {}
 
         // 重载函数
         T const& operator*() const;
@@ -67,8 +67,18 @@ class LinkedList {
     bool is_empty() { return _size == 0; }
 
     // 获取首项与末项元素检测
-    T& front() { if (_size == 0) { throw std::out_of_range("LinkedList: out of range"); } return _head->next()->val(); }
-    T& back() { if (_size == 0) { throw std::out_of_range("LinkedList: out of range"); } return _foot->prev()->val(); }
+    T& front() {
+        if (_size == 0) {
+            throw std::out_of_range("LinkedList: out of range");
+        }
+        return _head->next()->val();
+    }
+    T& back() {
+        if (_size == 0) {
+            throw std::out_of_range("LinkedList: out of range");
+        }
+        return _foot->prev()->val();
+    }
 
     // 首节点(尾节点)下(上)一节点设置
     void push_front(T const& val);
@@ -84,81 +94,97 @@ class LinkedList {
     Iterator find(T const& val);
 };
 
-
 /*
  * 内部 Node 访问器函数
  * 保持访问器函数的简洁, 所有边界检查依靠自行
  */
 
-template<typename T>
-T const& LinkedList<T>::Node::val() const { return this->_value; }
-template<typename T>
-T& LinkedList<T>::Node::val() { return this->_value; }
+template <typename T>
+T const& LinkedList<T>::Node::val() const {
+    return this->_value;
+}
+template <typename T>
+T& LinkedList<T>::Node::val() {
+    return this->_value;
+}
 
-template<typename T>
-LinkedList<T>::Node* const& LinkedList<T>::Node::next() const { return this->_next; }
-template<typename T>
-LinkedList<T>::Node* &LinkedList<T>::Node::next() { return this->_next; }
+template <typename T>
+LinkedList<T>::Node* const& LinkedList<T>::Node::next() const {
+    return this->_next;
+}
+template <typename T>
+LinkedList<T>::Node*& LinkedList<T>::Node::next() {
+    return this->_next;
+}
 
-template<typename T>
-LinkedList<T>::Node* const& LinkedList<T>::Node::prev() const { return this->_prev; }
-template<typename T>
-LinkedList<T>::Node* &LinkedList<T>::Node::prev() { return this->_prev; }
+template <typename T>
+LinkedList<T>::Node* const& LinkedList<T>::Node::prev() const {
+    return this->_prev;
+}
+template <typename T>
+LinkedList<T>::Node*& LinkedList<T>::Node::prev() {
+    return this->_prev;
+}
 
-
-/* 
+/*
  * 内部迭代器 Iterator 重载函数
  */
 
 // 该函数使用前必须确保值有意义
-template<typename T>
-T const& LinkedList<T>::Iterator::operator*() const { return this->_ptr->val(); }
-template<typename T>
-T& LinkedList<T>::Iterator::operator*() { return this->_ptr->val(); }
+template <typename T>
+T const& LinkedList<T>::Iterator::operator*() const {
+    return this->_ptr->val();
+}
+template <typename T>
+T& LinkedList<T>::Iterator::operator*() {
+    return this->_ptr->val();
+}
 
-template<typename T>
+template <typename T>
 LinkedList<T>::Iterator& LinkedList<T>::Iterator::operator++() {
     _ptr = (_ptr != _foot ? _ptr->next() : _ptr);
     return *this;
 }
 
-template<typename T>
+template <typename T>
 LinkedList<T>::Iterator LinkedList<T>::Iterator::operator++(int) {
     LinkedList<T>::Iterator temp(*this);
     _ptr = (_ptr != _foot ? _ptr->next() : _ptr);
     return temp;
 }
 
-template<typename T>
+template <typename T>
 LinkedList<T>::Iterator& LinkedList<T>::Iterator::operator--() {
     _ptr = (_ptr->prev() != _head ? _ptr->prev() : _ptr);
     return *this;
 }
 
-template<typename T>
+template <typename T>
 LinkedList<T>::Iterator LinkedList<T>::Iterator::operator--(int) {
     LinkedList<T>::Iterator temp(*this);
     _ptr = (_ptr->prev() != _head ? _ptr->prev() : _ptr);
     return *this;
 }
 
-template<typename T>
-bool LinkedList<T>::Iterator::operator==(LinkedList<T>::Iterator const& rhs) const {
+template <typename T>
+bool LinkedList<T>::Iterator::operator==(
+    LinkedList<T>::Iterator const& rhs) const {
     return (this->_ptr == rhs._ptr);
 }
 
-template<typename T>
-bool LinkedList<T>::Iterator::operator!=(LinkedList<T>::Iterator const& rhs) const {
+template <typename T>
+bool LinkedList<T>::Iterator::operator!=(
+    LinkedList<T>::Iterator const& rhs) const {
     return (this->_ptr != rhs._ptr);
 }
 
-
-/* 
+/*
  * LinkedList 基本构造函数与析构函数
  */
 
-template<typename T>
-LinkedList<T>::LinkedList(size_t size) : _head(new Node()), _foot(new Node()), _size(size) {
+template <typename T>
+LinkedList<T>::LinkedList(size_t size)
+    : _head(new Node()), _foot(new Node()), _size(size) {
     Node* prev = _head;
     while (size--) {
         Node* next = new Node();
@@ -167,8 +193,9 @@ LinkedList<T>::LinkedList(size_t size) : _head(new Node()), _foot(new Node()), _
     }
     prev->next() = _foot, _foot->prev() = prev;
 }
-template<typename T>
-LinkedList<T>::LinkedList(size_t size, T const& val) : _head(new Node()), _foot(new Node()), _size(size) {
+template <typename T>
+LinkedList<T>::LinkedList(size_t size, T const& val)
+    : _head(new Node()), _foot(new Node()), _size(size) {
     Node* prev = _head;
     while (size--) {
         Node* next = new Node(val);
@@ -178,90 +205,94 @@ LinkedList<T>::LinkedList(size_t size, T const& val) : _head(new Node()), _foot(
     prev->next() = _foot, _foot->prev() = prev;
 }
 
-template<typename T>
+template <typename T>
 LinkedList<T>::~LinkedList() {
-    for (Node* p = _head; p != nullptr; ) { Node* q = p->next(); delete p; p = q; }
+    for (Node* p = _head; p != nullptr;) {
+        Node* q = p->next();
+        delete p;
+        p = q;
+    }
     this->_size = 0, this->_head = nullptr, this->_foot = nullptr;
 }
 
-
-/* 
+/*
  * C 风格数组操作(附有边界检查)
  */
 
-template<typename T>
+template <typename T>
 T& LinkedList<T>::operator[](size_t index) {
     if (index >= this->_size) {
-        throw std::out_of_range("LinkedList: out of range"); 
+        throw std::out_of_range("LinkedList: out of range");
     }
     Node* temp = _head->next();
-    while(index--) { temp = (temp->next()); }
+    while (index--) {
+        temp = (temp->next());
+    }
     return temp->val();
 }
 
-
-/* 
+/*
  * 首节点(尾节点)下(上)一节点设置
  */
 
-template<typename T>
+template <typename T>
 void LinkedList<T>::push_front(T const& val) {
-    Node* ptr = new Node(val), *temp = _head->next();
-    _head->next() = ptr, ptr->next() = temp,
-    ptr->prev() = _head, temp->prev() = ptr;
+    Node *ptr = new Node(val), *temp = _head->next();
+    _head->next() = ptr, ptr->next() = temp, ptr->prev() = _head,
+    temp->prev() = ptr;
     _size++;
 }
 
-template<typename T>
+template <typename T>
 void LinkedList<T>::push_back(T const& val) {
-    Node* ptr = new Node(val), *temp = _foot->prev();
-    _foot->prev() = ptr, ptr->prev() = temp,
-    ptr->next() = _foot, temp->next() = ptr;
+    Node *ptr = new Node(val), *temp = _foot->prev();
+    _foot->prev() = ptr, ptr->prev() = temp, ptr->next() = _foot,
+    temp->next() = ptr;
     _size++;
 }
-
 
 /*
  * 节点插入设置
  */
 
-template<typename T>
+template <typename T>
 void LinkedList<T>::insert(size_t index, T const& val) {
     if (index > this->_size) {
-        throw std::out_of_range("LinkedList: out of range"); 
+        throw std::out_of_range("LinkedList: out of range");
     }
-    Node* temp = _head->next(), *new_node = new Node(val);
-    while (index--) { temp = (temp->next()); }
+    Node *temp = _head->next(), *new_node = new Node(val);
+    while (index--) {
+        temp = (temp->next());
+    }
     Node* ltemp = temp->prev();
-    ltemp->next() = new_node, new_node->prev() = ltemp,
-    new_node->next() = temp, temp->prev() = new_node;
+    ltemp->next() = new_node, new_node->prev() = ltemp, new_node->next() = temp,
+    temp->prev() = new_node;
     _size++;
 }
-
 
 /*
  * 节点删除设置
  */
 
-template<typename T>
+template <typename T>
 void LinkedList<T>::remove(size_t index) {
     if (index >= this->_size) {
-        throw std::out_of_range("LinkedList: out of range"); 
+        throw std::out_of_range("LinkedList: out of range");
     }
     Node* temp = _head->next();
-    while (index--) { temp = (temp->next()); }
-    temp->prev()->next() = temp->next(), 
-    temp->next()->prev() = temp->prev();
+    while (index--) {
+        temp = (temp->next());
+    }
+    temp->prev()->next() = temp->next(), temp->next()->prev() = temp->prev();
     delete temp;
     _size--;
 }
-
 
 /*
  * 节点查找设置
  */
 
-template<typename T>
+template <typename T>
 LinkedList<T>::Iterator LinkedList<T>::find(T const& val) {
     for (auto it = this->begin(); it != this->end(); ++it) {
         if (*it == val) {
